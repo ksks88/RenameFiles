493352ad28b5664809c949f30203d98cb3d6a92f
Salih Hajlakovic <salih.hajlakovic@gmail.com>
2017-07-21 01:10:08 +0200

    Initial checking

--- RenameFiles/Program.cs
+++ RenameFiles/Program.cs
@@ -6,7 +6,49 @@ namespace RenameFiles
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            bool success = false;
+            string equalsSign = new string('=', 40);
+            string prelude = "Please select the rename type: ";
+            string renameTypes = "\n1. Numbering - create a numbered list \n2. Custom word";
+            string introText = String.Format("{0}\n{1}\n{2}\n{0}", equalsSign, prelude, renameTypes);
+
+            Console.WriteLine(introText);
+
+            int input = 0;
+            while (!int.TryParse(Console.ReadLine(), out input) || input == 0 || input > 2)
+            {
+                Console.ForegroundColor = ConsoleColor.DarkRed;
+                Console.WriteLine("Please enter valid input!");
+                Console.ForegroundColor = ConsoleColor.White;
+            }
+
+            Console.WriteLine("{0}\nPlease enter root folder path:\n", equalsSign);
+            string folderPath = Console.ReadLine();
+
+            Console.WriteLine("\n{0}\nPlease enter file(s) extension(*.png;*.jpeg; etc.)  :\n", equalsSign);
+            string extension = Console.ReadLine();
+            switch (input)
+            {
+                case 1:
+                    success = RenameFiles.Instance.RenameToNumberedList(folderPath, extension);
+                    break;
+                case 2:
+                    break;
+                default:
+                    break;
+            }
+            if (success)
+            {
+                Console.ForegroundColor = ConsoleColor.DarkGreen;
+                Console.WriteLine("Success!");
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.DarkRed;
+                Console.WriteLine("Failed.");
+            }
+            Console.WriteLine("END");
+            Console.Read();
         }
     }
 }
---
+++ RenameFiles/RenameFiles.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace RenameFiles
+{
+    public sealed class RenameFiles
+    {
+        #region Singleton
+
+        private static readonly RenameFiles _instance = new RenameFiles();
+        public static RenameFiles Instance
+        {
+            get
+            {
+                return _instance;
+            }
+        }
+
+        // Explicit static constructor to tell C# compiler
+        // not to mark type as beforefieldinit
+        static RenameFiles()
+        {
+
+        }
+        private RenameFiles()
+        {
+        }
+
+        #endregion
+
+        #region Methods
+        public bool RenameToNumberedList(string folderPath, string extension)
+        {
+            try
+            {
+                var directories = CustomSearcher.GetDirectories(folderPath, "*", SearchOption.AllDirectories);
+
+                if (directories == null)
+                    directories = new List<string>() { folderPath };
+                else
+                    directories.Add(folderPath);
+
+                int counter = 1;
+                foreach (var item in directories)
+                {
+                    Console.ForegroundColor = ConsoleColor.DarkGreen;
+                    Console.WriteLine("Running items in folder: " + item + "\n");
+
+                    try
+                    {
+                        foreach (string filePath in Directory.GetFiles(item, extension))
+                        {
+                            try
+                            {
+                                string filename = filePath.Split(new char[] { '\\' }, StringSplitOptions.RemoveEmptyEntries).Last();
+                                string newFilePath = String.Format("{0}\\{1}. {2}", item, counter.ToString(), filename);
+                                Console.WriteLine("Renaming {0} to {1}...\n", filePath, newFilePath);
+                                if (File.Exists(newFilePath))
+                                {
+                                    System.IO.File.Delete(newFilePath);
+                                }
+
+                                System.IO.File.Move(filePath, newFilePath);
+
+                                counter++;
+                            }
+                            catch (Exception ex)
+                            {
+                                Console.ForegroundColor = ConsoleColor.DarkRed;
+                                Console.WriteLine(ex.ToString());
+                                Console.ForegroundColor = ConsoleColor.DarkGreen;
+                            }
+
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.ForegroundColor = ConsoleColor.DarkRed;
+                        Console.WriteLine(ex.ToString());
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.ForegroundColor = ConsoleColor.DarkRed;
+                Console.WriteLine(ex.ToString());
+                return false;
+            }
+
+            return true;
+        }
+        #endregion
+
+    }
+}
